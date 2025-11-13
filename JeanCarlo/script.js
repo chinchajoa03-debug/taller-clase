3b1c044566d6c3ddb4491d5ae1e4ba781ad6ca61
jeanc <chinchajoa@gmail.com>
2025-11-13 12:36:20 -0500

    feat se cualcula cuanto perimetro tiene un potrero rectangular

--- JeanCarlo/script.js
+++ JeanCarlo/script.js
@@ -1,13 +1,13 @@
-//ejercicio 2
+//ejercicio 3
 
-let ancho = parseFloat(prompt("Ingresa el ancho de la pared en metros:"));
-let alto = parseFloat(prompt("Ingresa el alto de la pared en metros:"));
+let largo = parseFloat(prompt("Ingresa el largo del potrero en metros:"));
+let ancho = parseFloat(prompt("Ingresa el ancho del potrero en metros:"));
 
-if (isNaN(ancho) || isNaN(alto) || ancho <= 0 || alto <= 0) {
+if (isNaN(largo) || isNaN(ancho) || largo <= 0 || ancho <= 0) {
     alert("Por favor ingresa valores numéricos válidos y mayores a cero.");
 } else {
 
-    let area = ancho * alto;
+    let perimetro = 2 * (largo + ancho);
 
-    alert(`El área de la pared es de ${area} metros cuadrados.`);
+    alert(`El perímetro del potrero es de ${perimetro.toFixed(2)} metros.`);
 }

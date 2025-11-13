905dbc9bdcb89fae25dfd4a029f78600f40c14c6
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-11-13 11:47:25 -0500

    feat: se calcula el total de compra con tres productos usando prompt y alert

---
+++ JeanCarlo/script.js
@@ -0,0 +1,10 @@
+// ejercicio 1
+let leche = parseFloat(prompt("Ingrese el precio del primer producto:"));
+let arroz = parseFloat(prompt("Ingrese el precio del segundo producto:"));
+let papa = parseFloat(prompt("Ingrese el precio del tercer producto:"));
+
+
+let total = leche + arroz + papa;
+
+
+alert("El total a pagar es: $" + total);

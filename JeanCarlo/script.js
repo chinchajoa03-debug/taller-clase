5873a5d4bda64a21691fdf4e51f262a54a7362d1
jeanc <chinchajoa03@gmail.com>
2025-11-18 22:30:22 -0500

    feat se crea programa que pide el ancho y alto de una pared y calcula su area

--- JeanCarlo/script.js
+++ JeanCarlo/script.js
@@ -1,10 +1,18 @@
 //1.
+/*
+let producto1= parseFloat(prompt("Ingrese el precio del primer producto"))
+let producto2= parseFloat(prompt("Ingrese el precio del segundo producto"))
+let producto3= parseFloat(prompt("Ingrese el precio del tercer producto"))
 
-let producto1 = parseFloat(prompt("Ingrese el precio del primer producto"));
-let producto2 = parseFloat(prompt("Ingrese el precio del segundo producto"));
-let producto3 = parseFloat(prompt("Ingrese el precio del tercer producto"));
+  let total = producto1 + producto2 + producto3
 
-  let total = producto1 + producto2 + producto3;
+  console.log("El total de tu compra es: $" + total)
+*/
+//2.
 
-  console.log("El total de tu compra es: $" + total);
+let alto= parseFloat(prompt("ingrese el alto de la pared"))
+let ancho= parseFloat(prompt("ingrese el ancho de la pared"))
 
+let area= ancho*alto
+
+console.log("el area es: " + area)

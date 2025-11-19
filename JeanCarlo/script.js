5189ba9359ca897f55b9876570d154e2fdd9f7fc
jeanc <chinchajoa03@gmail.com>
2025-11-18 22:14:10 -0500

    feat se crea un programa que calcula el valor total de tres productos}

---
+++ JeanCarlo/script.js
@@ -0,0 +1,10 @@
+//1.
+
+let producto1 = parseFloat(prompt("Ingrese el precio del primer producto"));
+let producto2 = parseFloat(prompt("Ingrese el precio del segundo producto"));
+let producto3 = parseFloat(prompt("Ingrese el precio del tercer producto"));
+
+  let total = producto1 + producto2 + producto3;
+
+  console.log("El total de tu compra es: $" + total);
+

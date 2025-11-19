bfa4b57e9cfc8950c442f8a62f5f494c1dc59ec9
jeanc <chinchajoa03@gmail.com>
2025-11-18 22:38:20 -0500

    feat se crea un programa que lee el largo y ancho de un potrero y calcula el perimetro

--- JeanCarlo/script.js
+++ JeanCarlo/script.js
@@ -7,12 +7,19 @@ let producto3= parseFloat(prompt("Ingrese el precio del tercer producto"))
   let total = producto1 + producto2 + producto3
 
   console.log("El total de tu compra es: $" + total)
-*/
-//2.
+//2.-----------------------------------------------------------------------------
 
-let alto= parseFloat(prompt("ingrese el alto de la pared"))
-let ancho= parseFloat(prompt("ingrese el ancho de la pared"))
+let altoPared= parseFloat(prompt("ingrese el alto de la pared"))
+let anchoPared= parseFloat(prompt("ingrese el ancho de la pared"))
 
-let area= ancho*alto
+let area= anchoPared*altoPared
 
-console.log("el area es: " + area)
+console.log("el area es: " + area)*/
+//3.-----------------------------------------------------------------------------
+
+let largoPotrero= parseFloat(prompt("ingrese el largo del potrero"))
+let anchoPotrero= parseFloat(prompt("ingrese el ancho del potrero"))
+
+let perimetro= 2*(largoPotrero+anchoPotrero)
+
+console.log("el perimetro del potrero rectangular es: " + perimetro)
